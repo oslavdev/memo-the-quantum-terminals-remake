99b206bd957f7065aa1c257c9972732bd8547fb0
Jaroslaw <yaroslav@xuver-services.com>
2021-08-20 16:48:46 +0300

    Unit test paths

--- __tests__/config/paths.ts
+++ __tests__/config/paths.ts
@@ -1,11 +1,29 @@
 import {
-  pathHome
-} from "@/app/config/paths";
+  pathHome,
+  pathLobby,
+  pathLogin,
+  pathMenu,
+  pathRegister,
+} from '@/app/config/paths'
 
-describe("test paths", () => {
-  
-  it("Path home", () => {
-    expect(pathHome()).toBe("/")
+describe('test paths', () => {
+  it('Path home', () => {
+    expect(pathHome()).toBe('/')
   })
 
+  it('Path menu', () => {
+    expect(pathMenu()).toBe('/menu')
+  })
+
+  it('Path lobby', () => {
+    expect(pathLobby()).toBe('/lobby')
+  })
+
+  it('Path login', () => {
+    expect(pathLogin()).toBe('/login')
+  })
+
+  it('Path Register', () => {
+    expect(pathRegister()).toBe('/register')
+  })
 })
