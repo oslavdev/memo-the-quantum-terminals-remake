317cbbcb17be07476c2cbc44e83d240e40dfe9f4
Jaroslaw <yaroslav@xuver-services.com>
2021-08-20 17:07:19 +0300

    Fix User type

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -1,12 +1,9 @@
-
-
 export type UserType = {
-    id: string
-    username: string
-    email: string
-    created_at: Date
-    firstName?: string
-    secondName?: string
-    ip?: string
-    password?: string
+  id: string
+  username: string
+  email: string
+  created_at: Date
+  firstName?: string
+  secondName?: string
+  password?: string
 }
