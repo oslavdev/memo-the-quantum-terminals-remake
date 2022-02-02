3be21a1c6ca5bbdd5bb61905b492b54854b1a4e7
Jaroslaw Grishunin <41283085+oslavdev@users.noreply.github.com>
2022-02-02 15:25:28 +0100

    chore: extend dangerfile with more rules

--- dangerfile.js
+++ dangerfile.js
@@ -15,7 +15,7 @@ const env_files = created_files.find((file) =>
 )
 if(env_files){
     fail(
-        `This PR contains \`.env\` files. Please remove files before mergin this PR.`
+        `🙈 This PR contains \`.env\` files. Please remove files before mergin this PR.`
       )
 }
 
@@ -35,8 +35,34 @@ message(`:tada: The PR added ${additions} and removed ${deletions} lines.`)
 
 /* If it's not a branch PR */
 if (pr.base.repo.full_name !== pr.head.repo.full_name) {
-    warn("This PR comes from a fork, and won't get JS generated for QA testing this PR inside the Emission Example app.")
+    warn("🕵️ This PR comes from a fork, and won't get JS generated for QA testing this PR inside the Emission Example app.")
 }
 
+/** No lock file */
+const packageChanged = danger.git.modified_files.includes('package.json');
+const lockfileChanged = danger.git.modified_files.includes('yarn.lock');
 
-// TODO: check release version, tests
+if (packageChanged && !lockfileChanged) {
+    warn(`Changes were made to package.json, but not to yarn.lock - <i>'Perhaps you need to run yarn?'</i>`);
+}
+
+/** Doesnt match format */
+danger.git.commits.forEach(commit => {
+    if (!commit.message.match(/^(feat:)|(fix:)|(major:)|(chore:)/g)) {
+      fail(`Commit message '${commit.message}' does match the correct format`);
+    }
+})
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/* ~ Achievemnts                                                            ~ */
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+/**
+ * Rule: Celebrate PRs that remove more code than they add.
+ * Reason: Less is more!
+ */
+ if (danger.github.pr.deletions > danger.github.pr.additions) {
+    message(
+      `👏 Great job! I see more lines deleted than added. Thanks for keeping us lean!`
+    )
+  }
