36e841e51eacfca9f4cbc43a1658bfe10c236d69
Jaroslaw Grishunin <41283085+oslavdev@users.noreply.github.com>
2022-02-04 11:09:35 +0100

    feat: open memo game directly from the lobby

--- src/app.tsx
+++ src/app.tsx
@@ -9,7 +9,7 @@ import reducer from '@/app/reducers/root'
 import { theme } from '@/UI/config/theme'
 
 /* Games */
-import Memo from '@/pages/Game/Memo' // memorize paths
+import Memo from '@/pages/Memo'
 
 /* Pages */
 import Login from '@/pages/Login'
@@ -25,25 +25,13 @@ import { ModalProvider } from '@/context/confirmModal'
 import { ErrorhandleProvider } from '@/context/error'
 import { StartGameProvider } from '@/context/startGame'
 import { MusicManagerContextProvider } from '@/context/music'
-import Loading from '@/UI/Loading/Loading'
-import LayoutMenu from '@/components/Layout/Layout_menu'
-import { Box } from '@/UI/Boxes/Box'
+
 /* Fragments */
 import Modals from '@/fragments/Modals'
 
 /** Pages */
-import Terminals from '@/pages/Terminals'
-import Wrong from './components/Wrong'
-
-function LoadingFallback(){
-  return(
-    <LayoutMenu logo={false}>
-    <Box position="absolute" top="50%" center>
-      <Loading />
-    </Box>
-  </LayoutMenu>
-  )
-}
+import Wrong from '@/components/something-wrong'
+import { LoadingFallback } from '@/components/loading-fallback'
 
 const App = () => {
   const initialState = useContext(State)
@@ -61,38 +49,34 @@ const App = () => {
                   <Routes>
 
                   <Route path={path.pathLobby()} element={<PrivateRoute/>}>
-                        <Route element={<Lobby/>}/>
+                        <Route path={path.pathLobby()} element={<Lobby/>}/>
                   </Route>
 
-                  <Route path={path.pathTerminals()} element={<PrivateRoute/>}>
-                        <Route element={<Terminals/>}/>
-                  </Route>
-
-                  <Route path={path.pathTerminal('memo')} element={<PrivateRoute/>}>
-                        <Route element={<Memo/>}/>
+                  <Route path={path.pathMemo()} element={<PrivateRoute/>}>
+                        <Route path={path.pathMemo()} element={<Memo/>}/>
                   </Route>
 
                   <Route path={path.pathRegister()} element={<PublicRoute/>}>
-                        <Route element={<Register/>}/>
+                        <Route path={path.pathRegister()} element={<Register/>}/>
                   </Route>
 
                   <Route path={path.pathLogin()} element={<PublicRoute/>}>
-                        <Route element={<Login/>}/>
+                        <Route path={path.pathLogin()} element={<Login/>}/>
                   </Route>
 
                   <Route path={path.pathHome()} element={<PublicRoute/>}>
-                        <Route element={<Menu/>}/>
+                        <Route path={path.pathHome()} element={<Menu/>}/>
                   </Route>
 
                   <Route path={path.pathActivationSent()} element={<PublicRoute/>}>
-                        <Route element={<ActivationSent/>}/>
+                        <Route path={path.pathActivationSent()} element={<ActivationSent/>}/>
                   </Route>
 
                   <Route path={path.pathActivation()} element={<PublicRoute/>}>
-                        <Route element={<Activation/>}/>
+                        <Route path={path.pathActivation()} element={<Activation/>}/>
                   </Route>
 
-                     <Route path={'/unknown-error'} element={<Wrong/>}/>
+                    <Route path={'/unknown-error'} element={<Wrong/>}/>
                     <Route path={'/not-found'} element={<NotFoundPage/>}/>
                     <Route path="*" element={<NotFoundPage/>} />
                   </Routes>
