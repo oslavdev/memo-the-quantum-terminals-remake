53f2589b370523c63d209a4f3be2916573a7eb6c
Jaroslaw Grishunin <41283085+oslavdev@users.noreply.github.com>
2022-08-25 15:00:30 +0200

    fix: temporarily remove sound manager

--- src/components/Layout/Layout_menu.tsx
+++ src/components/Layout/Layout_menu.tsx
@@ -1,9 +1,7 @@
-import React, {useEffect, useState} from 'react'
-import Sound from 'react-sound';
+import React from 'react'
 import { Box } from '@/UI/Boxes/Box'
 import Snow from '@/components/Miscellaneous/Snow'
 import Logo from '@/components/Miscellaneous/Logo'
-import {useMusicManagerDispatch, useMusicManagerState, MENU_THEME, STOP_ALL_MUSIC} from "@/context/music"
 
 export default function LayoutMenu({
   children,
@@ -13,45 +11,9 @@ export default function LayoutMenu({
   logo?: boolean
 }) {
 
-  const musicManagerState = useMusicManagerState();
-  const musicManagerDispatch = useMusicManagerDispatch();
-
-  const [musicState, setMusicState] = useState<string>("loading"); // loading | playing | stopped
-
-  useEffect(() =>{
-    musicManagerDispatch({
-      type: MENU_THEME
-    })
-  }, [])
-
-  const handleLoadingSong = () =>{
-    setMusicState("loading")
-  }
-
-  const handlePlayingSong = () =>{
-    setMusicState("playing")
-  }
-
-  const handleStoppedSong = () =>{
-    setMusicState("stopped")
-    musicManagerDispatch({
-      type: STOP_ALL_MUSIC
-    })
-  }
-
 
   return (
     <>
-    {musicManagerState && musicManagerState.play && (
-      <Sound
-        url={musicManagerState.src}
-        playStatus={musicManagerState.play ? "PLAYING" : "STOPPED"}
-        onLoading={handleLoadingSong}
-        onPlaying={handlePlayingSong}
-        autoLoad={true}
-        loop={true}
-      />
-    )}
     <Box
       position="fixed"
       w="100%"
@@ -72,12 +34,10 @@ export default function LayoutMenu({
         w="450px"
         h="100%"
       >
-        {logo ? <Logo /> : null}
+        {logo && <Logo />}
         {children}
-
       </Box>
       <Snow />
-
     </Box>
     </>
   )
